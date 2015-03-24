2c4f0f1ed3e86795d50b648c402f4c21c06121f5
utwang <x2x2tone@gmail.com>
2015-03-24 19:29:51 +0900

    rails generate paper_trail:install & migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150119105711) do
+ActiveRecord::Schema.define(version: 20150324102847) do
+
+  create_table "comments", force: true do |t|
+    t.integer  "post_id"
+    t.string   "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+
+  create_table "posts", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "taggables", force: true do |t|
+    t.integer  "post_id"
+    t.integer  "tag_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "taggables", ["post_id"], name: "index_taggables_on_post_id"
+  add_index "taggables", ["tag_id"], name: "index_taggables_on_tag_id"
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "turtles", force: true do |t|
     t.string   "name"
@@ -19,5 +50,16 @@ ActiveRecord::Schema.define(version: 20150119105711) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "versions", force: true do |t|
+    t.string   "item_type",  null: false
+    t.integer  "item_id",    null: false
+    t.string   "event",      null: false
+    t.string   "whodunnit"
+    t.text     "object"
+    t.datetime "created_at"
+  end
+
+  add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
 
 end
